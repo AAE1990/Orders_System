131f7098c3e61063ff14c61cdcad7e05495689c7
AAE1990 <anton.akhmadeev@yandex.ru>
2020-10-25 22:21:04 +0500

    Updates

---
+++ precache-manifest.1fca0a4d885f518c4ea79ac57285432c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b01628d1b73989743d32569689caa4",
+    "url": "/AAE1990/Orders_system/index.html"
+  },
+  {
+    "revision": "fce2979a31644d301670",
+    "url": "/AAE1990/Orders_system/static/css/2.86da2101.chunk.css"
+  },
+  {
+    "revision": "b947d037f0c9985bfddc",
+    "url": "/AAE1990/Orders_system/static/css/main.dba1dd65.chunk.css"
+  },
+  {
+    "revision": "fce2979a31644d301670",
+    "url": "/AAE1990/Orders_system/static/js/2.c7780e96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AAE1990/Orders_system/static/js/2.c7780e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b947d037f0c9985bfddc",
+    "url": "/AAE1990/Orders_system/static/js/main.870b0aa4.chunk.js"
+  },
+  {
+    "revision": "042680d92305a8d6b825",
+    "url": "/AAE1990/Orders_system/static/js/runtime-main.53c1e3ef.js"
+  }
+]);
